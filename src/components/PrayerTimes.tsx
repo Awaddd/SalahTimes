5360fa9440e36c5a602ee7273f7a592af8b5e41f
Awad <awad@moblox.com>
2021-11-01 23:17:59 +0000

    Added reset date

--- src/components/PrayerTimes.tsx
+++ src/components/PrayerTimes.tsx
@@ -1,12 +1,15 @@
 import { useQuery } from 'react-query'
 import { getPrayerTimes } from '../services/getPrayerTimesService';
 import { dateState } from '../state/global';
-import { useRecoilValue } from 'recoil';
-import { Loader } from '@mantine/core';
+import { useRecoilValue, useResetRecoilState } from 'recoil';
+
+import { Loader, ActionIcon } from '@mantine/core';
+import { FiRefreshCw } from 'react-icons/fi'
 
 const PrayerTimes = () => {
 
   const date = useRecoilValue(dateState)
+  const resetDate = useResetRecoilState(dateState)
 
   const { error, isLoading, data } = useQuery('prayerTimes', getPrayerTimes)
   if (error) return <p className="font-semibold text-red-400">Failed to fetch prayer times, please try again later</p>
@@ -18,8 +21,12 @@ const PrayerTimes = () => {
   return (
     <div className="w-full p-4 text-gray-200 bg-gray-900 rounded-sm md:px-6">
       <h4 className="font-semibold text-center">London</h4>
-      <h3 className="text-center text-blue-400">{date.format('DD MMMM')}</h3>
-      
+      <h3 className="relative text-center text-blue-400">
+        <span className="mr-2">{date.format('DD MMMM')}</span>
+        <ActionIcon variant="transparent" className="absolute inline text-gray-200" onClick={resetDate}>
+          <FiRefreshCw />
+        </ActionIcon>
+      </h3>
       <main className="grid grid-cols-2 px-2 mt-4">
         <ul className="grid gap-1">
           <li>Fajr</li>
