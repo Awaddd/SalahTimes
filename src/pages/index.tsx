cb9259c534def4fca0cb3bd6b834fc60d35b0a01
Awad <awad@moblox.com>
2021-11-01 23:04:51 +0000

    Navigation buttons appear disabled on reaching the start/end of month

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,26 @@ import { Meta } from '../layout/Meta';
 import { Main } from '../templates/Main';
 import PrayerTimes from '../components/PrayerTimes'
 import { dateState } from '../state/global';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
+import { useEffect, useState } from 'react';
 
+import clsx from 'clsx';
 import { Button } from '@mantine/core';
 import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
 
 const Index = () => {
 
-  const setDate = useSetRecoilState(dateState)
+  const [date, setDate] = useRecoilState(dateState)
+  const [disabled, setDisabled] = useState<'prev' | 'next' | 'none'>('none')
 
   const deincrementDate = () => setDate(date => date.date() > 1 ? date.subtract(1, 'day') : date)
-
   const incrementDate = () => setDate(date => date.date() < date.daysInMonth() ? date.add(1, 'day') : date)
 
+  useEffect(() => {
+    if (date.date() === 1) setDisabled('prev')
+    else if (date.date() === date.daysInMonth()) setDisabled('next')
+    else if (disabled !== 'none' && (disabled === 'prev' || disabled === 'next')) setDisabled('none')
+  }, [date, disabled])
 
   return (
     <Main meta={<Meta />}>
@@ -29,10 +36,10 @@ const Index = () => {
           </section>
           <section className="grid grid-cols-2 gap-2 mt-2">
             <div className="grid">
-              <Button leftIcon={<FiArrowLeft />} className="bg-gray-900 rounded-sm h-9" onClick={deincrementDate}>Previous</Button>
+              <Button leftIcon={<FiArrowLeft />} className={clsx(activeButton, disabled === 'prev' && disabledButton)} onClick={deincrementDate}>Previous</Button>
             </div>
             <div className="grid">
-              <Button rightIcon={<FiArrowRight />} className="bg-gray-900 rounded-sm h-9" onClick={incrementDate}>Next</Button>
+              <Button rightIcon={<FiArrowRight />} className={clsx(activeButton, disabled === 'next' && disabledButton)} onClick={incrementDate}>Next</Button>
             </div>
           </section>
         </div>
@@ -40,5 +47,8 @@ const Index = () => {
     </Main>
   );
 };
+
+const activeButton = "bg-gray-900 rounded-sm h-9"
+const disabledButton ="bg-gray-darkest"
 
 export default Index;
