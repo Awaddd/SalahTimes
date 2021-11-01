471e5ecae46aaa31dc648aa8bebef076570f8304
Awad <awad@moblox.com>
2021-11-01 14:13:00 +0000

    Moved data fetch to the prayer times component
    Also added loader

--- src/components/PrayerTimes.tsx
+++ src/components/PrayerTimes.tsx
@@ -1,6 +1,12 @@
-const PrayerTimes = ({ prayerTimes }: { prayerTimes: any }) => {
+import { useQuery } from 'react-query'
+import { getPrayerTimes } from '../services/getPrayerTimesService';
+import { Loader } from '@mantine/core';
 
-  console.log(prayerTimes)
+const PrayerTimes = () => {
+
+  const { error, isLoading, data } = useQuery('prayerTimes', getPrayerTimes)
+  if (error) return <p className="font-semibold text-red-400">Failed to fetch prayer times, please try again later</p>
+  console.log(data)
 
   return (
     <div className="w-full p-4 text-gray-200 bg-gray-900 rounded-sm md:px-6">
@@ -14,17 +20,27 @@ const PrayerTimes = ({ prayerTimes }: { prayerTimes: any }) => {
           <li>Maghrib</li>
           <li>Isha</li>
         </ul>
-        <ul className="grid gap-1 justify-self-end">
-          <li>05:12</li>
-          <li>06:49</li>
-          <li>11:49</li>
-          <li>14:07</li>
-          <li>16:38</li>
-          <li>18:05</li>
-        </ul>
+        {isLoading ? placeholder : (
+          <ul className="grid gap-1 justify-self-end">
+            <li>05:12</li>
+            <li>06:49</li>
+            <li>11:49</li>
+            <li>14:07</li>
+            <li>16:38</li>
+            <li>18:05</li>
+          </ul>
+        )}
       </main>
     </div>
   )
 }
+
+const placeholder = (
+  <ul className="grid gap-1 justify-self-end">
+    {[0, 0, 0, 0, 0, 0].map((_, k) => (
+      <li key={k}><Loader size="sm" color="white" variant="dots" /></li>
+    ))}
+  </ul>
+)
 
 export default PrayerTimes
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
 import { Meta } from '../layout/Meta';
 import { Main } from '../templates/Main';
-import { useQuery } from 'react-query'
 import PrayerTimes from '../components/PrayerTimes'
 
 import { Button } from '@mantine/core';
 import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
-import { getPrayerTimes } from '../services/getPrayerTimesService';
 
-const Index = (props: { prayerTimes: any }) => {
-
-  const { data } = useQuery('prayerTimes', getPrayerTimes, { initialData: props.prayerTimes })
+const Index = () => {
 
   return (
     <Main meta={<Meta />}>
@@ -20,7 +16,7 @@ const Index = (props: { prayerTimes: any }) => {
             <h3 className="font-semibold">05:12</h3>
           </section>
           <section className="mt-6 md:mt-8">
-            <PrayerTimes prayerTimes={data} />
+            <PrayerTimes />
           </section>
           <section className="grid grid-cols-2 gap-2 mt-2">
             <div className="grid">
@@ -35,10 +31,5 @@ const Index = (props: { prayerTimes: any }) => {
     </Main>
   );
 };
-
-export async function getServerSideProps() {
-  const prayerTimes = await getPrayerTimes()
-  return { props: { prayerTimes } }
-}
 
 export default Index;
