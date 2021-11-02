39aad6141a6ed7b3066dde0557d89e363ba80af2
Awad <awad@moblox.com>
2021-11-02 15:18:39 +0000

    Apis now work on prod

--- src/services/getPrayerTimesService.ts
+++ src/services/getPrayerTimesService.ts
@@ -1,2 +1,2 @@
-
-export const getPrayerTimes = () => fetch('http://localhost:3000/api/prayer-times').then(res => res.json())
+const baseURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'https://salahtimes.vercel.app/'
+export const getPrayerTimes = () => fetch(`${baseURL}/api/prayer-times`).then(res => res.json())
