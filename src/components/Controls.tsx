500e7cf3bfa554305a2366e3894eeeb5e025a1d8
Awad <awad@moblox.com>
2021-11-02 12:47:12 +0000

    Extracted prayer times controls to its own component

---
+++ src/components/Controls.tsx
@@ -0,0 +1,38 @@
+import { dateState } from '../state/global';
+import { useRecoilState } from 'recoil';
+import { useEffect, useState } from 'react';
+
+import clsx from 'clsx';
+import { Button } from '@mantine/core';
+import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
+
+function Controls() {
+
+  const [date, setDate] = useRecoilState(dateState)
+  const [disabled, setDisabled] = useState<'prev' | 'next' | 'none'>('none')
+
+  const deincrementDate = () => setDate(date => date.date() > 1 ? date.subtract(1, 'day') : date)
+  const incrementDate = () => setDate(date => date.date() < date.daysInMonth() ? date.add(1, 'day') : date)
+
+  useEffect(() => {
+    if (date.date() === 1) setDisabled('prev')
+    else if (date.date() === date.daysInMonth()) setDisabled('next')
+    else if (disabled !== 'none' && (disabled === 'prev' || disabled === 'next')) setDisabled('none')
+  }, [date, disabled])
+
+  return (
+    <>
+      <div className="grid">
+        <Button leftIcon={<FiArrowLeft />} className={clsx(activeButton, disabled === 'prev' && disabledButton)} onClick={deincrementDate}>Previous</Button>
+      </div>
+      <div className="grid">
+        <Button rightIcon={<FiArrowRight />} className={clsx(activeButton, disabled === 'next' && disabledButton)} onClick={incrementDate}>Next</Button>
+      </div>
+    </>
+  )
+}
+
+const activeButton = "bg-gray-900 rounded-sm h-9"
+const disabledButton ="bg-gray-darkest"
+
+export default Controls
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,9 @@
 import { Meta } from '../layout/Meta';
 import { Main } from '../templates/Main';
 import PrayerTimes from '../components/PrayerTimes'
-import { dateState } from '../state/global';
-import { useRecoilState } from 'recoil';
-import { useEffect, useState } from 'react';
-
-import clsx from 'clsx';
-import { Button } from '@mantine/core';
-import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
+import Controls from '../components/Controls';
 
 const Index = () => {
-
-  const [date, setDate] = useRecoilState(dateState)
-  const [disabled, setDisabled] = useState<'prev' | 'next' | 'none'>('none')
-
-  const deincrementDate = () => setDate(date => date.date() > 1 ? date.subtract(1, 'day') : date)
-  const incrementDate = () => setDate(date => date.date() < date.daysInMonth() ? date.add(1, 'day') : date)
-
-  useEffect(() => {
-    if (date.date() === 1) setDisabled('prev')
-    else if (date.date() === date.daysInMonth()) setDisabled('next')
-    else if (disabled !== 'none' && (disabled === 'prev' || disabled === 'next')) setDisabled('none')
-  }, [date, disabled])
-
   return (
     <Main meta={<Meta />}>
       <main className="h-full px-3 text-gray-200 md:py-10">
@@ -35,20 +16,12 @@ const Index = () => {
             <PrayerTimes />
           </section>
           <section className="grid grid-cols-2 gap-2 mt-2">
-            <div className="grid">
-              <Button leftIcon={<FiArrowLeft />} className={clsx(activeButton, disabled === 'prev' && disabledButton)} onClick={deincrementDate}>Previous</Button>
-            </div>
-            <div className="grid">
-              <Button rightIcon={<FiArrowRight />} className={clsx(activeButton, disabled === 'next' && disabledButton)} onClick={incrementDate}>Next</Button>
-            </div>
+            <Controls />
           </section>
         </div>
       </main>
     </Main>
   );
 };
-
-const activeButton = "bg-gray-900 rounded-sm h-9"
-const disabledButton ="bg-gray-darkest"
 
 export default Index;
